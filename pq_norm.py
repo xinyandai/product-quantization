f1c309caa268a2aeeebf5c4bff4b22ef58c908b9
xinyandai <381675152@qq.com>
2018-11-10 11:33:38 +0800

    NormPQ: 1. generate norm intervals with kmeans(a little bit better than uniformly intervals, uniformly intervals is a little bit better than percentiles); 2. remove bug: approximate norm rather than approximate norm.

--- pq_norm.py
+++ pq_norm.py
@@ -31,10 +31,13 @@ class NormPQ(object):
         np.random.seed(seed)
 
         norms, normalized_vecs = normalize(vecs)
-        # float64 here -> float64
-        self.percentiles = np.percentile(norms, np.linspace(0, 100, self.n_percentile + 1)[:])
-        self.percentiles = np.array(self.percentiles, dtype=np.float32)
 
+        # self.percentiles = np.percentile(norms, np.linspace(0, 100, self.n_percentile + 1)[:])
+        # self.percentiles = np.linspace(np.min(norms), np.max(norms), self.n_percentile + 1)
+        # float64 here -> float64
+        # self.percentiles = np.array(self.percentiles, dtype=np.float32)
+
+        self.percentiles, _ = kmeans2(norms[:, np.newaxis], self.n_percentile, iter=iter, minit='points')
         self.quantize.fit(normalized_vecs, iter, seed)
 
         return self
@@ -50,27 +53,32 @@ class NormPQ(object):
         norms, normalized_vecs = normalize(vecs)
         codes = self.quantize.encode(normalized_vecs)
 
-        norm_index = [np.argmax(self.percentiles[1:] > n) for n in norms]
-        norm_index = np.clip(norm_index, 1, self.n_percentile)
+        # norm_index = [np.argmax(self.percentiles[1:] > n) for n in norms]
+        # norm_index = np.clip(norm_index, 1, self.n_percentile)
+        norm_index, _ = vq(norms[:, np.newaxis], self.percentiles)
 
         return codes, norm_index
 
     def decode(self, codes, norm_index, norms=None):
         assert codes.dtype == self.code_dtype
 
+        if not self.true_norm:
+            # norms = (self.percentiles[norm_index]+self.percentiles[norm_index-1]) / 2.0
+            norms = self.percentiles[norm_index, 0]
+
         vecs = self.quantize.decode(codes)
         _, vecs = normalize(vecs)
 
-        if not self.true_norm:
-            norms = (self.percentiles[norm_index]+self.percentiles[norm_index-1]) / 2.0
         assert norms is not None
         return (vecs.transpose() * norms).transpose()  # can only apply broadcast on columns, so transpose is needed
 
     def compress(self, vecs):
         norms, normalized_vecs = normalize(vecs)
-        if self.true_norm :
-            norms = np.linalg.norm(vecs, axis=1)
+        if not self.true_norm:
+            _, norm_index = self.encode(vecs)
+            norms = self.percentiles[norm_index, 0]
 
         compressed_vecs = self.quantize.compress(normalized_vecs)
+        _, compressed_vecs = normalize(compressed_vecs)
 
         return (compressed_vecs.transpose() * norms).transpose()
