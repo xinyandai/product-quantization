21b2464ef984dbe48464e9efebbd10ab739f8f93
xinyandai <381675152@qq.com>
2018-11-12 11:48:40 +0800

    Fix bug, wrong configuration for rq..

--- script/aq.sh
+++ script/aq.sh
@@ -4,7 +4,8 @@ cd ..
 codebook=$1
 dataset=$3
 
-log=script/$1_codebook_$(basename "$0").log
+mkdir script/$3
+log=script/$3/$1_$2_$(basename "$0").log
 echo witing into file : $log
 
 python3 ./run.py \
--- script/norm_pq.sh
+++ script/norm_pq.sh
@@ -3,8 +3,10 @@ cd ..
 
 codebook=`expr $1 - 1`
 norm_centroid=$2
+dataset=$3
 
-log=script/$1_codebook_$(basename "$0").log
+mkdir script/$3
+log=script/$3/$1_$2_$(basename "$0").log
 echo witing into file : $log
 
 python3 ./run.py \
