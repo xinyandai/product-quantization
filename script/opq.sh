75b65fb205d9efab7c39343bc8ef235faf3c442b
xinyandai <381675152@qq.com>
2018-11-15 20:53:12 +0800

    Support opq.

---
+++ script/opq.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+cd ..
+
+codebook=$1
+dataset=$3
+
+mkdir script/$3/$4
+log=script/$3/$4/$1_$2_$(basename "$0").log
+echo witing into file : $log
+
+python3 ./run.py \
+    -q opq \
+    --dataset ${dataset} \
+    --topk $4 \
+    --metric product \
+    --ranker product \
+    --num_codebook ${codebook} \
+    --layer 1 \
+    --Ks 256 \
+    > $log
