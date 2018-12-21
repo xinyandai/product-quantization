47cba2cd4a0a3058a7f3112fbb0eafa7b70453c2
xinyandai <381675152@qq.com>
2018-12-21 17:25:23 +0800

    Run script for apq and rq.

---
+++ run_norm_rq.py
@@ -0,0 +1,19 @@
+from vecs_io import *
+from pq_residual import *
+from sorter import *
+from run import execute
+
+
+if __name__ == '__main__':
+    import sys
+    dataset = sys.argv[1]
+    topk = int(sys.argv[2])
+    codebook = int(sys.argv[3])
+    Ks = int(sys.argv[4])
+
+    X, Q, G = loader(dataset, topk, 'product')
+    # pq, rq, or component of norm-pq
+    pqs = [PQ(M=1, Ks=Ks) for _ in range(3)]
+    quantizer = ResidualPQ(pqs=pqs)
+    quantizer = NormPQ(n_percentile=codebook, quantize=quantizer, method='kmeans_partial')
+    execute(quantizer, X, Q, G, 'product')
