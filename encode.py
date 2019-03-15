8d38b6ae829b8f3c0e4800e51006c8a996087670
Kelvin Ng <nkwkelvin@gmail.com>
2019-03-15 15:42:58 +0800

    A script for encoding with a trained model (#6)

---
+++ encode.py
@@ -0,0 +1,37 @@
+from vecs_io import *
+import math
+import argparse
+import pickle
+import numpy as np
+
+
+def gen_chunk(X, chunk_size=1000000):
+    for i in range(math.ceil(len(X) / chunk_size)):
+        yield X[i * chunk_size : (i + 1) * chunk_size]
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process input method and parameters.')
+    parser.add_argument('--dataset', type=str, help='choose data set name')
+    parser.add_argument('--data_type', type=str, default='fvecs', help='data type of base and queries')
+    parser.add_argument('--topk', type=int, help='topk of ground truth')
+    parser.add_argument('--metric', type=str, help='metric of ground truth, euclid by default')
+
+    parser.add_argument('--save_dir', type=str, help='dir to save results', default='./results')
+    parser.add_argument('--result_suffix', type=str, help='suffix to be added to the file names of the results', default='')
+
+    parser.add_argument('--chunk_size', type=int, help='chunk size', default=1000000)
+
+    args = parser.parse_args()
+
+    X, _, _, _ = mmap_loader(args.dataset, args.topk, args.metric, data_type=args.data_type)
+
+    np.random.seed(123)
+
+    with open(args.save_dir + '/' + args.dataset + args.result_suffix + '.pickle', 'rb') as f:
+        quantizer = pickle.load(f)
+
+    with open(args.save_dir + '/' + args.dataset + args.result_suffix + '_encoded', 'wb') as f:
+        for X_chunk in gen_chunk(X, args.chunk_size):
+            codes = quantizer.encode(np.ascontiguousarray(X_chunk, np.float32))
+            codes.tofile(f)
