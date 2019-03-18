16012ae23bc5d16d0559cb57f1aed3cf50c38322
Kelvin Ng <nkwkelvin@gmail.com>
2019-03-18 17:15:28 +0800

    Extend generate_imi_buckets.py to support any number of imi size and dim

--- generate_imi_buckets.py
+++ generate_imi_buckets.py
@@ -5,22 +5,26 @@ import numba as nb
 
 
 @nb.njit(parallel=True)
-def generateIMICodes(imiSize):
-    codes = np.empty((imiSize * imiSize, 2), dtype=np.uint8)
+def generateIMICodes(imiSize, imiDim, code_dtype):
+    codes = np.empty((imiSize ** imiDim, imiDim), dtype=code_dtype)
 
-    for i in nb.prange(imiSize * imiSize):
-        codes[i, 0] = i / imiSize
-        codes[i, 1] = i % imiSize
+    for i in nb.prange(codes.shape[0]):
+        cur_code = i + 0
+        for j in range(imiDim - 1, -1, -1):
+            codes[i, j] = cur_code % imiSize
+            cur_code //= imiSize
 
     return codes
 
 
 @nb.njit(parallel=True)
-def imiId2BucketId(imiIds, imiSize):
-    bucketIds = np.empty(imiIds.shape[0], dtype=np.uint64)
+def imiId2BucketId(imiIds, imiSize, imiDim):
+    bucketIds = np.zeros(imiIds.shape[0], dtype=np.uint64)
 
     for i in nb.prange(imiIds.shape[0]):
-        bucketIds[i] = imiIds[i][0] * imiSize + imiIds[i][1]
+        for j in range(imiDim):
+            bucketIds[i] *= imiSize
+            bucketIds[i] += imiIds[i, j]
 
     return bucketIds
 
@@ -28,6 +32,9 @@ def imiId2BucketId(imiIds, imiSize):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process input method and parameters.')
     parser.add_argument('--dataset', type=str, help='choose data set name')
+
+    parser.add_argument('--imiSize', type=int, help='number of codes per IMI dimension', default=256)
+    parser.add_argument('--imiDim', type=int, help='IMI dimension (1 means IVFADC)', default=2)
 
     parser.add_argument('--save_dir', type=str, help='dir to save results', default='./results')
     parser.add_argument('--result_suffix', type=str, help='suffix to be added to the file names of the results', default='')
@@ -42,18 +49,17 @@ if __name__ == '__main__':
     with open(args.save_dir + '/' + args.dataset + args.result_suffix + '_encoded', 'rb') as f:
         codes = np.fromfile(f, dtype=quantizer.code_dtype).reshape(-1, quantizer.num_codebooks)
 
-    codesIMI = codes[:, :2]
-    imiSize = 256
-    bucketIds = imiId2BucketId(codesIMI, imiSize)
+    codesIMI = codes[:, :args.imiDim]
+    bucketIds = imiId2BucketId(codesIMI, args.imiSize, args.imiDim)
 
     itemIds = np.argsort(bucketIds).astype(np.uint64)
     bucketIdsSorted = bucketIds[itemIds]
 
-    bucketLengths = np.bincount(bucketIdsSorted.astype(np.int64)).astype(np.uint32) # TODO: bucketIdsSorted should not be converted to int64, seems to be a bug of numpy
+    bucketLengths = np.bincount(bucketIdsSorted.astype(np.int64), minlength=args.imiSize ** args.imiDim).astype(np.uint32) # TODO: bucketIdsSorted should not be converted to int64, seems to be a bug of numpy
 
     bucketOffsets = np.concatenate(([0], np.cumsum(bucketLengths)[:-1]))
 
-    imiCenters = quantizer.decode(generateIMICodes(imiSize))
+    imiCenters = quantizer.decode(generateIMICodes(args.imiSize, args.imiDim, codesIMI.dtype))
 
     np.savez(args.save_dir + '/' + args.dataset + args.result_suffix + '_imi.npz', 
              itemIds=itemIds,
