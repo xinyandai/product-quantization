6672bfb6ced703c8aabd21658712e82d3f5f5550
xinyan <xinyan.dai@outlook.com>
2019-03-21 17:18:30 +0000

    Dev Iteratively RQ.

---
+++ pq_residual_iteratively.py
@@ -0,0 +1,90 @@
+from __future__ import division
+from __future__ import print_function
+from pq_norm import *
+import tqdm
+from scipy.cluster.vq import vq, _vq, kmeans2
+
+def increase_kmeans(data, code_book, iter):
+    nc = len(code_book)
+    for i in range(iter):
+        # Compute the nearest neighbor for each obs using the current code book
+        label = vq(data, code_book)[0]
+        # Update the code book by computing centroids
+        new_code_book, has_members = _vq.update_cluster_means(data, label, nc)
+        if not has_members.all():
+            # Set the empty clusters to their previous positions
+            new_code_book[~has_members] = code_book[~has_members]
+        code_book = new_code_book
+
+    return code_book, label
+
+
+class IterativelyResidualPQ(object):
+    def __init__(self, deep, Ks=256, verbose=True):
+        self.verbose = verbose
+        self.deep = deep
+        self.Ks = Ks
+        self.codewords = None
+        self.code_dtype = np.uint8 if Ks <= 2 ** 8 else (np.uint16 if Ks <= 2 ** 16 else np.uint32)
+
+    def class_message(self):
+        return "IterativelyResidualPQ layers: {}".format(self.deep)
+
+    @property
+    def num_codebooks(self):
+        return self.M * self.deep
+
+    def fit(self, T, iter, save_codebook=False, save_decoded=[], save_residue_norms=[], save_results_T=False, dataset_name=None, save_dir=None, D=None):
+        assert T.dtype == np.float32
+        assert T.ndim == 2
+        N, D = np.shape(T)
+        residual = np.empty(shape=(N, D), dtype=T.dtype)
+        compressed = np.empty(shape=(self.deep, N, D), dtype=T.dtype)
+        self.codewords = np.empty(shape=(self.deep, self.Ks, D))
+
+        residual[:, :] = T[:, :]
+        for m in tqdm.tqdm(range(self.deep)):
+
+            self.codewords[m, :, :], codes = kmeans2(residual, self.Ks, iter=iter, minit='points')
+            compressed[m, :, :] = self.codewords[m, codes, :]
+            residual = residual - compressed[m, :, :]
+
+        for _ in tqdm.tqdm(range(iter)):
+            for m in range(self.deep):
+                layer_residual = T[:, :] - (np.sum(compressed, axis=0) - compressed[m])
+
+                self.codewords[m, :, :], codes_m = increase_kmeans(layer_residual, code_book=self.codewords[m, :, :], iter=iter)
+                compressed[m, :, :] = self.codewords[m, codes_m, :]
+
+
+        return self
+
+    def encode(self, vecs):
+        """
+        :param vecs:
+        :return: (N, deep * M)
+        """
+        print("# iteratively ecoding")
+        N, D = np.shape(vecs)
+        codes = np.zeros((N, self.deep), dtype=self.code_dtype)  # N * deep * M
+        residual = vecs.copy()
+        for m in range(self.deep):
+            codes[:, m] = vq(residual, self.codewords[m])[0]
+            residual[:, :] = residual - self.codewords[m, codes[:, m], :]
+        print("# iteratively ecoding")
+        compressed = np.empty(shape=(self.deep, N, D), dtype=vecs.dtype)
+        for _ in tqdm.tqdm(range(20)):
+            for m in range(self.deep):
+                layer_residual = vecs[:, :] - (np.sum(compressed, axis=0) - compressed[m])
+                codes_m = vq(layer_residual, code_book=self.codewords[m, :, :])[0]
+                compressed[m, :, :] = self.codewords[m, codes_m, :]
+        #        codes[:, m] = codes_m
+        return codes
+
+    def decode(self, codes, left=0, right=None):
+
+        vecss = [self.codewords[m, codes[:, m]] for m in range(self.deep)]
+        return np.sum(vecss, axis=0)
+
+    def compress(self, X):
+        return self.decode(self.encode(X))
---
+++ run_iteratively_rq.py
@@ -0,0 +1,38 @@
+from vecs_io import *
+from pq_residual_iteratively import *
+from sorter import *
+from run_pq import execute
+from run_pq import parse_args
+import pickle
+
+
+if __name__ == '__main__':
+    dataset = 'netflix'
+    topk = 20
+    codebook = 3
+    Ks = 256
+    metric = 'product'
+
+    # override default parameters with command line parameters
+    import sys
+    args = parse_args(dataset, topk, codebook, Ks, metric)
+    print("# Parameters: dataset = {}, topK = {}, codebook = {}, Ks = {}, metric = {}"
+          .format(args.dataset, args.topk, args.num_codebook, args.Ks, args.metric))
+
+    X, T, Q, G = loader(args.dataset, args.topk, args.metric, folder=args.data_dir)
+    if T is None:
+        T = X[:args.train_size]
+    else:
+        T = T[:args.train_size]
+    T = np.ascontiguousarray(T, np.float32)
+
+    # pq, rq, or component of norm-pq
+    pqs = [PQ(M=1, Ks=args.Ks) for _ in range(args.num_codebook)]
+    quantizer = IterativelyResidualPQ(deep=codebook)
+    if args.rank:
+        execute(quantizer, X, T, Q, G, args.metric)
+    if args.save_model:
+        if not args.rank:
+            quantizer.fit(T, iter=20)
+        with open(args.save_dir + '/' + args.dataset + '_rq' + args.result_suffix + '.pickle', 'wb') as f:
+            pickle.dump(quantizer, f)
